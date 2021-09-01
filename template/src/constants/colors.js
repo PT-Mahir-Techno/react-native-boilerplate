421fecc63019307b78c1640fd9f3cfb6e6921d68
AbidKhairyAK <abid.khairy.ak@gmail.com>
2021-09-01 19:20:15 +0700

    change colors constants

--- template/src/constants/colors.js
+++ template/src/constants/colors.js
@@ -3,15 +3,19 @@ const colors = {
 	black: '#333333',
 	white: '#FFFFFF',
 
+	darkGray: '#3D405B',
 	gray: '#67697C',
 	shallowGray: '#A3A5A7',
 	lightGray: '#C7C9DC',
 	dullWhite: '#F3F5F7',
 	
 	green: '#6EC149',
-	yellow: '#FFAD05',
+	yellow: '#FFAE03',
 	red: '#E84855',
 	blue: '#3185FC',
+	pink: '#FF6392',
+	orange: '#F06330',
+	violet: '#541388',
 }
 
 export default colors
